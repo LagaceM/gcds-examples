39ed9713d51869c8b78fee7984d6c5dd6607d772
Lagace, Mathieu <Mathieu.Lagace@dfo-mpo.gc.ca>
2026-07-29 08:43:19 -0400

    works now

--- starter-apps/angular/angular-template/src/app/app.config.server.ts
+++ starter-apps/angular/angular-template/src/app/app.config.server.ts
@@ -1,30 +1,17 @@
-import {
-  APP_ID,
-  ApplicationConfig,
-  DOCUMENT,
-  inject,
-  mergeApplicationConfig,
-  provideAppInitializer,
-} from "@angular/core";
+import { APP_ID, ApplicationConfig, DOCUMENT, inject, mergeApplicationConfig } from "@angular/core";
 import { BEFORE_APP_SERIALIZED } from "@angular/platform-server";
 import { provideServerRendering, withRoutes } from "@angular/ssr";
 import { hydrateDocument } from "@gcds-core/components/hydrate";
-import { defineCustomElements } from "@gcds-core/components/loader";
 import { appConfig } from "./app.config";
 import { serverRoutes } from "./app.routes.server";
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideAppInitializer(() => defineCustomElements()),
     {
       provide: BEFORE_APP_SERIALIZED,
-      useFactory: () => {
-        const doc = inject(DOCUMENT);
-        const appId = inject(APP_ID);
-
-        return () => hydrateGcdsComponents(doc, appId);
-      },
+      useFactory: hydrateIonicComponents,
       multi: true,
+      deps: [DOCUMENT, APP_ID],
     },
     provideServerRendering(withRoutes(serverRoutes)),
   ],
@@ -33,10 +20,9 @@ const serverConfig: ApplicationConfig = {
 export const config = mergeApplicationConfig(appConfig, serverConfig);
 
 export function hydrateGcdsComponents(doc: Document, appId: string) {
-  console.debug("hydrateGcdsComponents", doc, appId);
-
   return hydrateDocument(doc, {
     serializeShadowRoot: "scoped",
+    clientHydrateAnnotations: true,
   }).then((hydrateResults) => {
     hydrateResults.diagnostics.forEach((d) => {
       if (d.type === "error") {
@@ -49,4 +35,75 @@ export function hydrateGcdsComponents(doc: Document, appId: string) {
     });
     return hydrateResults;
   });
+}
+
+export function hydrateIonicComponents() {
+  const doc = inject(DOCUMENT);
+  const appId = inject(APP_ID);
+
+  return () => {
+    const supportsNativeAttachShadow =
+      typeof doc?.createElement?.('div')?.attachShadow === 'function';
+
+    return hydrateDocument(doc, {
+      // Fallback for SSR DOMs (e.g. Domino) that do not implement attachShadow.
+      ...(supportsNativeAttachShadow
+        ? { clientHydrateAnnotations: false }
+        : {
+            serializeShadowRoot: 'scoped',
+            clientHydrateAnnotations: true,
+          }),
+      excludeComponents: [
+        // overlays
+        'ion-action-sheet',
+        'ion-alert',
+        'ion-loading',
+        'ion-modal',
+        'ion-picker-legacy',
+        'ion-popover',
+        'ion-toast',
+        'ion-toast',
+
+        // navigation
+        'ion-router',
+        'ion-route',
+        'ion-route-redirect',
+        'ion-router-link',
+        'ion-router-outlet',
+
+        // tabs
+        'ion-tabs',
+        'ion-tab',
+
+        // auxiliar
+        'ion-picker-legacy-column',
+      ],
+    }).then((hydrateResults) => {
+      hydrateResults.diagnostics.forEach((d) => {
+        if (d.type === 'error') {
+          console.error(d.messageText);
+        } else if (d.type === 'debug') {
+          console.debug(d.messageText);
+        } else {
+          console.log(d.messageText);
+        }
+      });
+
+      if (doc.head != null) {
+        const styleElms = doc.head.querySelectorAll('style[data-styles]') as NodeListOf<HTMLStyleElement>;
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let i = 0; i < styleElms.length; i++) {
+          styleElms[i].setAttribute('ng-transition', appId);
+        }
+      }
+
+      if (doc.body != null) {
+        const ionPages = doc.body.querySelectorAll('.ion-page.ion-page-invisible') as NodeListOf<HTMLElement>;
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let i = 0; i < ionPages.length; i++) {
+          ionPages[i].classList.remove('ion-page-invisible');
+        }
+      }
+    });
+  };
 }
--- starter-apps/angular/angular-template/src/app/app.config.ts
+++ starter-apps/angular/angular-template/src/app/app.config.ts
@@ -1,18 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { isPlatformBrowser } from "@angular/common";
 import {
   ApplicationConfig,
+  DOCUMENT,
+  NgZone,
+  PLATFORM_ID,
+  inject,
   provideAppInitializer,
   provideZoneChangeDetection,
 } from "@angular/core";
-import { provideClientHydration } from "@angular/platform-browser";
+import { provideClientHydration, withEventReplay } from "@angular/platform-browser";
 import { provideRouter } from "@angular/router";
 import { defineCustomElements } from "@gcds-core/components/loader";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAppInitializer(() => defineCustomElements()),
+    provideAppInitializer(() => {
+      const doc = inject(DOCUMENT);
+      const zone = inject(NgZone);
+
+      if (
+        isPlatformBrowser(inject(PLATFORM_ID)) &&
+        doc.defaultView &&
+        typeof window !== "undefined"
+      ) {
+        doc.querySelectorAll("style[sty-id]").forEach((el) => el.setAttribute("sty-id", ""));
+
+        const aelFn =
+          "__zone_symbol__addEventListener" in (doc.body as any)
+            ? "__zone_symbol__addEventListener"
+            : "addEventListener";
+
+        return defineCustomElements(doc.defaultView, {
+          syncQueue: true,
+          jmp: (h: any) => zone.runOutsideAngular(h),
+          ael(elm, eventName, cb, opts) {
+            (elm as any)[aelFn](eventName, cb, opts);
+          },
+          rel(elm, eventName, cb, opts) {
+            elm.removeEventListener(eventName, cb, opts);
+          },
+        });
+      }
+    }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
+    provideClientHydration(withEventReplay()),
   ],
 };
